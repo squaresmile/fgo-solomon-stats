bdd58b836cb8ef3755ecddbef4d0a12409103d68
squaresmile <squaresmile@protonmail.com>
2019-03-04 15:19:04 -0500

    Format code with black

--- app.py
+++ app.py
@@ -6,152 +6,158 @@ import plotly.graph_objs as go
 from flask_caching import Cache
 
 boss_list = [
-    'flauros',
-    'forneus',
-    'barbatos',
-    'halphas',
-    'amon_ra',
-    'sabnock',
-    'andromalius'
+    "flauros",
+    "forneus",
+    "barbatos",
+    "halphas",
+    "amon_ra",
+    "sabnock",
+    "andromalius",
 ]
 boss_color = {
-    'flauros': '#d62728',
-    'forneus': '#9467bd',
-    'barbatos': '#1f77b4',
-    'halphas': '#ff7f0e',
-    'amon_ra': '#bcbd22',
-    'sabnock': '#7f7f7f',
-    'andromalius': '#e377c2'
+    "flauros": "#d62728",
+    "forneus": "#9467bd",
+    "barbatos": "#1f77b4",
+    "halphas": "#ff7f0e",
+    "amon_ra": "#bcbd22",
+    "sabnock": "#7f7f7f",
+    "andromalius": "#e377c2",
 }
 boss_name = {
-    'flauros':'Flauros',
-    'forneus': 'Forneus',
-    'barbatos': 'Barbatos',
-    'halphas': 'Halphas',
-    'amon_ra': 'Amon Ra',
-    'sabnock': 'Sabnock',
-    'andromalius': 'Andromalius'
+    "flauros": "Flauros",
+    "forneus": "Forneus",
+    "barbatos": "Barbatos",
+    "halphas": "Halphas",
+    "amon_ra": "Amon Ra",
+    "sabnock": "Sabnock",
+    "andromalius": "Andromalius",
 }
 boss_interval = {
-    'flauros': 4,
-    'forneus': 4,
-    'barbatos': 1,
-    'halphas': 8,
-    'amon_ra': 8,
-    'sabnock': 4,
-    'andromalius': 3,
-    'all_kps': 8,
-    'all_kps_except_barbatos': 8,
-    'all_kills_counts': 8
+    "flauros": 4,
+    "forneus": 4,
+    "barbatos": 1,
+    "halphas": 8,
+    "amon_ra": 8,
+    "sabnock": 4,
+    "andromalius": 3,
+    "all_kps": 8,
+    "all_kps_except_barbatos": 8,
+    "all_kills_counts": 8,
 }
 chart_dict = {
-    'kps': {'name': 'KPS', 'unit': 'Kills per second'},
-    'hp': {'name': 'Kills Counts', 'unit': 'Kills'}
+    "kps": {"name": "KPS", "unit": "Kills per second"},
+    "hp": {"name": "Kills Counts", "unit": "Kills"},
 }
 
 boss_df = {}
 for boss in boss_list:
-    boss_df[boss] = pd.read_csv('output/{}.csv'.format(boss))
-#update_time = pd.to_datetime(boss_df['andromalius']['time'].iloc[-1])
+    boss_df[boss] = pd.read_csv("output/{}.csv".format(boss))
+# update_time = pd.to_datetime(boss_df['andromalius']['time'].iloc[-1])
 
-dash_boss = [{'value': boss_id, 'label': name} for boss_id, name in boss_name.items()]
-dash_chart = [{'value': chart, 'label': chart_dict[chart]['name']} for chart in chart_dict]
+dash_boss = [{"value": boss_id, "label": name} for boss_id, name in boss_name.items()]
+dash_chart = [
+    {"value": chart, "label": chart_dict[chart]["name"]} for chart in chart_dict
+]
 
 app = dash.Dash(__name__)
 app.title = "NA Solomon raid statistic"
-cache = Cache(app.server, config={
-    'CACHE_TYPE': 'redis',
-    'CACHE_REDIS_URL': 'redis://localhost:6379'
-})
+cache = Cache(
+    app.server,
+    config={"CACHE_TYPE": "redis", "CACHE_REDIS_URL": "redis://localhost:6379"},
+)
 app.config.suppress_callback_exceptions = True
-timeout = 3600 #cache timeout in seconds
+timeout = 3600  # cache timeout in seconds
 server = app.server
 
-app.layout = html.Div(children=[
-    html.Div(children=[
-        html.H1(children='NA Solomon raid statistic'),
-        html.A('Source data', href='https://atlasacademy.github.io/solomon/'),
-        html.Div(children=[
-            html.Div(children='Boss:', style={'font-weight': 'bold', 'display': 'inline-block'}),
-            dcc.Checklist(
-                id='boss_checklist',
-                options=dash_boss,
-                labelStyle={'display': 'inline-block'},
-                values=[b for b in boss_list if b != 'barbatos'],
-                style={'display': 'inline-block', 'margin-left': '5px'}
-            )
-        ]),
-        html.Div(children=[
-            html.Div(children='Statistic:', style={'font-weight': 'bold', 'display': 'inline-block'}),
-            dcc.RadioItems(
-                id='chart_value',
-                options=dash_chart,
-                labelStyle={'display': 'inline-block'},
-                value='kps',
-                style={'display': 'inline-block', 'margin-left': '5px'}
-            )
-        ]),
-        dcc.Graph(id='solomon_raid_graph', style={'height': '75vh'})
-    ], style={'textAlign': 'center'})
-]#, className="container"
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.H1(children="NA Solomon raid statistic"),
+                html.A("Source data", href="https://atlasacademy.github.io/solomon/"),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Boss:",
+                            style={"font-weight": "bold", "display": "inline-block"},
+                        ),
+                        dcc.Checklist(
+                            id="boss_checklist",
+                            options=dash_boss,
+                            labelStyle={"display": "inline-block"},
+                            values=[b for b in boss_list if b != "barbatos"],
+                            style={"display": "inline-block", "margin-left": "5px"},
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Statistic:",
+                            style={"font-weight": "bold", "display": "inline-block"},
+                        ),
+                        dcc.RadioItems(
+                            id="chart_value",
+                            options=dash_chart,
+                            labelStyle={"display": "inline-block"},
+                            value="kps",
+                            style={"display": "inline-block", "margin-left": "5px"},
+                        ),
+                    ]
+                ),
+                dcc.Graph(id="solomon_raid_graph", style={"height": "75vh"}),
+            ],
+            style={"textAlign": "center"},
+        )
+    ]  # , className="container"
 )
 
+
 @app.callback(
-    dash.dependencies.Output('solomon_raid_graph', 'figure'),
-    [dash.dependencies.Input('boss_checklist', 'values'),
-    dash.dependencies.Input('chart_value', 'value')]
+    dash.dependencies.Output("solomon_raid_graph", "figure"),
+    [
+        dash.dependencies.Input("boss_checklist", "values"),
+        dash.dependencies.Input("chart_value", "value"),
+    ],
 )
 @cache.memoize(timeout=timeout)
 def update_graph(chosen_boss, chosen_chart):
     if len(chosen_boss) == 1:
         boss_title = boss_name[chosen_boss[0]]
     else:
-        boss_title = 'Demon God Pillars'
+        boss_title = "Demon God Pillars"
     chart_list = []
     for boss in chosen_boss:
-        time = boss_df[boss]['time']
+        time = boss_df[boss]["time"]
         chart_list.append(
             go.Scatter(
-                x = time,
-                y = boss_df[boss][chosen_chart],
-                name = boss_name[boss],
-                line = dict(color = boss_color[boss])
+                x=time,
+                y=boss_df[boss][chosen_chart],
+                name=boss_name[boss],
+                line=dict(color=boss_color[boss]),
             )
         )
     layout = dict(
-        title = '{} {}'.format(boss_title, chart_dict[chosen_chart]['name']),
-        xaxis = dict(
-            title = 'Pacific Standard Time',
+        title="{} {}".format(boss_title, chart_dict[chosen_chart]["name"]),
+        xaxis=dict(
+            title="Pacific Standard Time",
             rangeselector=dict(
-                buttons=list([
-                    dict(count=12,
-                        label='12h',
-                        step='hour',
-                        stepmode='backward'),
-                    dict(count=1,
-                        label='1d',
-                        step='day',
-                        stepmode='backward'),
-                    dict(count=3,
-                        label='3d',
-                        step='day',
-                        stepmode='backward'),
-                    dict(step='all')
-                ])
+                buttons=list(
+                    [
+                        dict(count=12, label="12h", step="hour", stepmode="backward"),
+                        dict(count=1, label="1d", step="day", stepmode="backward"),
+                        dict(count=3, label="3d", step="day", stepmode="backward"),
+                        dict(step="all"),
+                    ]
+                )
             ),
-            rangeslider=dict(
-                visible = True
-            ),
-            type='date'
+            rangeslider=dict(visible=True),
+            type="date",
         ),
-        yaxis = dict(
-            title = chart_dict[chosen_chart]['unit']
-        )
+        yaxis=dict(title=chart_dict[chosen_chart]["unit"]),
     )
-    return {
-        'data': chart_list,
-        'layout': layout
-    }
+    return {"data": chart_list, "layout": layout}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run_server(debug=True)
--- imgur-upload.py
+++ imgur-upload.py
@@ -3,51 +3,54 @@ from imgurpython import ImgurClient
 import subprocess
 from datetime import datetime
 import json
-base_path = '/var/www/fgo-solomon-stats/output/'
+
+base_path = "/var/www/fgo-solomon-stats/output/"
 
 file_list = [
-    'all_kps.png',
-    'all_kps_except_barbatos.png',
-    'all_kills_counts.png',
-    'barbatos.png',
-    'forneus.png',
-    'flauros.png',
-    'sabnock.png',
-    'halphas.png',
-    'andromalius.png',
-    'amon_ra.png'
+    "all_kps.png",
+    "all_kps_except_barbatos.png",
+    "all_kills_counts.png",
+    "barbatos.png",
+    "forneus.png",
+    "flauros.png",
+    "sabnock.png",
+    "halphas.png",
+    "andromalius.png",
+    "amon_ra.png",
 ]
 file_list = [base_path + file for file in file_list]
 
-with open('imgur-api.json', 'r') as f:
+with open("imgur-api.json", "r") as f:
     account_credentials = json.load(f)
 
-client_id = account_credentials['client_id']
-client_secret = account_credentials['client_secret']
-access_token = account_credentials['access_token']
-refresh_token = account_credentials['refresh_token']
+client_id = account_credentials["client_id"]
+client_secret = account_credentials["client_secret"]
+access_token = account_credentials["access_token"]
+refresh_token = account_credentials["refresh_token"]
+
 
 def imgur_upload():
     client = ImgurClient(client_id, client_secret, access_token, refresh_token)
-    
-    albums = client.get_album_images('OKG9U3G')
-    
+
+    albums = client.get_album_images("OKG9U3G")
+
     current_image_id = [image.id for image in albums]
-    
+
     new_image_id = []
     for file in file_list:
-#        with open(os.devnull, "w") as f:
-#            subprocess.call(["ect", file], stdout=f)
-#        shutil.copy2(file, "/var/www/fgo.square.ovh/solomon-raid-stats/")
-        id = client.upload_from_path(file, anon=False)['id']
+        #        with open(os.devnull, "w") as f:
+        #            subprocess.call(["ect", file], stdout=f)
+        #        shutil.copy2(file, "/var/www/fgo.square.ovh/solomon-raid-stats/")
+        id = client.upload_from_path(file, anon=False)["id"]
         new_image_id.append(id)
     print("{}: Uploaded to imgur".format(datetime.now()))
-    
+
     for id in current_image_id:
-        client.album_remove_images('OKG9U3G', id)
-    
+        client.album_remove_images("OKG9U3G", id)
+
     for id in new_image_id:
-        client.album_add_images('OKG9U3G', id)
+        client.album_add_images("OKG9U3G", id)
     print("{}: Editted album".format(datetime.now()))
+
 
 imgur_upload()
